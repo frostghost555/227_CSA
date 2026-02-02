866610a9d49a98292597743cbf2c84bb77702322
frostghost555 <83512260+frostghost555@users.noreply.github.com>
2026-02-02 10:48:21 -0800

    Create MediaLib.java

---
+++ MediaLib.java
@@ -0,0 +1,26 @@
+/**
+ * Activity 2.2.7
+ * 
+ * A MediaLib class for the MediaLibrary program
+ */
+public class MediaLib
+{
+  private Book book;
+
+  public void addBook(Book b)
+  {
+    book = b;
+  }
+
+/*Modify toString to show the state
+* of the library
+* @precondition MediaLib not null
+*
+*/
+  public String toString() 
+  {
+    String info = "Book added! : \n" + book.toString();
+    
+    return info;
+  }
+}
