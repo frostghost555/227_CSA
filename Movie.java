e33aaeb5bccfcc288010b0bb4908dfadf1ed28ae
frostghost555 <83512260+frostghost555@users.noreply.github.com>
2026-02-03 23:24:29 -0800

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -7,15 +7,73 @@ public class Movie
 {
   //Objects, therefore mutated if ref changes
   private String title;
-  private String author;
+  private double duration;
   //Can only mutate by value
   private int rating;
   
   /*** Constructor ****/
-  public Movie(String t, String a)
+  public Movie(String t, double d)
   {
     title = t;
-    author = a;
+    duration = d;
     rating = 0;
+  }
+  
+   /*** Accessor methods ***/
+  public String getTitle() {
+    return title;
+  }
+
+  public double getDuration() {
+    return duration;
+  }
+  
+  public int getRating() {
+    return rating;
+  }
+  
+  public String toString() 
+  {
+    String info = "\"" + title + "\", duration is " + duration + " minutes";
+    if (rating != 0) 
+    { 
+      info += ", rating is " + rating;
+    }
+    return info;
+  }
+
+  public boolean equals(Movie m)
+  {
+    if (m == null){
+        return false;
+    }
+    return this.title.equals(m.title) && this.duration == m.duration;
+  }
+
+
+  /*** Mutator methods ***/
+  public void setTitle(String t) {
+    title = t;
+  }
+  public void setDuration(double d) {
+    duration = d;
+  }
+  public void adjustRating(int change)
+  {
+    int newRating = rating + change;
+
+    if (newRating >= 0 && newRating <= 10) {
+      rating = newRating;
+    }
+  }
+
+  /*
+  * Set Rating
+  *@param int rating = number between 0 and 10
+  *PostCondition: Rating attribute will be an integer
+  *PreCondition: Object cannot be null
+  */
+  public void setRating(int r){
+    rating = r;
   }
 }
